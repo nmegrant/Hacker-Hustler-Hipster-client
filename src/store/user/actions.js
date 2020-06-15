f81573e3f2f22f6f2a45049f8aa75ab9e4343eb0
nmegrant <nme.grant@gmail.com>
2020-06-15 21:52:12 +0200

    Set up login thunk to get token from API

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,5 +1,33 @@
 import axios from "axios";
 
+export function loginedIn(userAndToken) {
+  return {
+    type: "LOGGED_IN",
+    payload: userAndToken,
+  };
+}
+
+export function LoggedOut() {
+  return {
+    type: "LOGGED_OUT",
+    payload: null,
+  };
+}
+
+export function loginThunkCreator(email, password) {
+  return async function (dispatch, getState) {
+    try {
+      const response = await axios.post(`http://localhost:4000/login`, {
+        email,
+        password,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  };
+}
+
 export function signUpThunkCreator(newUser) {
   return async function signUpThunkCreator(dispatch, getState) {
     try {
@@ -7,7 +35,6 @@ export function signUpThunkCreator(newUser) {
         `http://localhost:4000/signup`,
         newUser
       );
-      console.log(response);
     } catch (error) {
       console.log(`Error: ${error}`);
     }
