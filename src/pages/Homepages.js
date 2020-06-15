0ebbdd41304b2b77a7a6c48aed4ed933fc26bdfe
nmegrant <nme.grant@gmail.com>
2020-06-15 10:31:48 +0200

    Homepages displayed on main page using Homepage Card to organize

--- src/pages/Homepages.js
+++ src/pages/Homepages.js
@@ -2,12 +2,30 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchHomepagesThunkCreator } from "../store/homepages/actions";
 import { selectHomepages } from "../store/homepages/selectors";
+import HomepageCard from "../components/HomepageCard";
 
 export default function Homepages() {
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(fetchHomepagesThunkCreator());
   }, [dispatch]);
 
-  return <h1>Hello!</h1>;
+  const homepages = useSelector(selectHomepages());
+
+  return (
+    <div>
+      {homepages.map((homepage) => {
+        return (
+          <HomepageCard
+            key={homepage.id}
+            userId={homepage.user.id}
+            name={homepage.user.name}
+            byline={homepage.byline}
+            tags={homepage.user.tags}
+          />
+        );
+      })}
+    </div>
+  );
 }
