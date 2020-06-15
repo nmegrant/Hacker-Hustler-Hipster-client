553478eb33e27195f8d5b176e6214a410cad7976
nmegrant <nme.grant@gmail.com>
2020-06-15 13:13:10 +0200

    Create selectors and reducer for homepageDetails

--- src/store/homepageDetails/actions.js
+++ src/store/homepageDetails/actions.js
@@ -1,12 +1,19 @@
 import axios from "axios";
 
+export function homepageDetailsFetched(details) {
+  return {
+    type: "FETCHED_DETAILS",
+    payload: details,
+  };
+}
+
 export function fetchHomepageDetailsThunkCreator(id) {
   return async function fetchHomepageDetailsThunk(dispatch, getState) {
     try {
       const homepageDetails = await axios.get(
         `http://localhost:4000/homepages/${id}`
       );
-      console.log(homepageDetails.data);
+      dispatch(homepageDetailsFetched(homepageDetails.data));
     } catch (error) {
       console.log(`Error: ${error}`);
     }
