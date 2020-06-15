11477e01e744650000d4c85f7cffd4790f4a3df9
nmegrant <nme.grant@gmail.com>
2020-06-15 13:13:10 +0200

    Create selectors and reducer for homepageDetails

--- src/pages/HomepageDetails .js
+++ src/pages/HomepageDetails .js
@@ -2,14 +2,18 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchHomepageDetailsThunkCreator } from "../store/homepageDetails/actions";
+import { selectHomepageDetails } from "../store/homepageDetails/selectors";
 
 export default function HomepageDetails() {
   const parameters = useParams();
   const dispatch = useDispatch();
+  const homepageDetail = useSelector(selectHomepageDetails());
 
   useEffect(() => {
     dispatch(fetchHomepageDetailsThunkCreator(parameters.homepageId));
   }, [dispatch, parameters.homepageId]);
+
+  console.log(homepageDetail);
 
   return (
     <div>
