8de21d70dd79416c4a5731c22d48215f729c8c59
nmegrant <nme.grant@gmail.com>
2020-06-15 22:10:42 +0200

    Add actions to log in and log out

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,21 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
 import { loginThunkCreator } from "../store/user/actions";
+import { selectToken } from "../store/user/selectors";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
+  const token = useSelector(selectToken());
+  const history = useHistory();
+
+  useEffect(() => {
+    if (token !== null) {
+      history.push("/");
+    }
+  }, [token, history]);
 
   function submitForm(event) {
     event.preventDefault();
