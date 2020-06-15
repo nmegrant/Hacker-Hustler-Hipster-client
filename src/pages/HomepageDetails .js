862806f0da4e855bc6d7c74cc2ac2678c88f4485
nmegrant <nme.grant@gmail.com>
2020-06-15 13:33:21 +0200

    Display homepage details

--- src/pages/HomepageDetails .js
+++ src/pages/HomepageDetails .js
@@ -7,17 +7,35 @@ import { selectHomepageDetails } from "../store/homepageDetails/selectors";
 export default function HomepageDetails() {
   const parameters = useParams();
   const dispatch = useDispatch();
-  const homepageDetail = useSelector(selectHomepageDetails());
+  const details = useSelector(selectHomepageDetails());
+
+  console.log(details);
 
   useEffect(() => {
     dispatch(fetchHomepageDetailsThunkCreator(parameters.homepageId));
   }, [dispatch, parameters.homepageId]);
 
-  console.log(homepageDetail);
-
   return (
     <div>
-      <h1>Details!!!!!</h1>
+      <h1>{details.user ? details.user.name : null}</h1>
+      <h2>{details.user ? details.user.role : null}</h2>
+      <h4>{details.byline}</h4>
+      <h3>{details.location}</h3>
+      {details.websites
+        ? details.websites.map((website) => {
+            return <p key={website.id}>{website.url}</p>;
+          })
+        : null}
+      <h4>Skills</h4>
+      {details.user
+        ? details.user.tags.map((tag) => {
+            return <h5 key={tag.id}>{tag.skill}</h5>;
+          })
+        : null}
+      <h4>Projects and Experience</h4>
+      <p>{details.experience}</p>
+      <h4>A bit more about me</h4>
+      <p>{details.bio}</p>
     </div>
   );
 }
