3e6797b3e1435942987544683c2630b075976cdd
nmegrant <nme.grant@gmail.com>
2020-06-15 12:41:31 +0200

    Create action thunk to fetch homepage details

--- src/pages/HomepageDetails .js
+++ src/pages/HomepageDetails .js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchHomepageDetailsThunkCreator } from "../store/homepageDetails/actions";
 
 export default function HomepageDetails() {
+  const parameters = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchHomepageDetailsThunkCreator(parameters.homepageId));
+  }, [dispatch, parameters.homepageId]);
+
   return (
     <div>
       <h1>Details!!!!!</h1>
