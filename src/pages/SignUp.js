ad310a2b4c80684b54c48e81b95f5f46fc07f2bc
nmegrant <nme.grant@gmail.com>
2020-06-16 12:37:26 +0200

    Add styling to forms for sign in and log in

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { signUpThunkCreator } from "../store/user/actions";
+
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Col } from "react-bootstrap";
 
 export default function SignUp() {
   const [name, setName] = useState("");
@@ -25,58 +31,85 @@ export default function SignUp() {
   }
 
   return (
-    <div>
-      <form>
-        <label>Name</label>
-        <input
-          type="text"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        ></input>
-        <label>Email</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        ></input>
-        <label>Password</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        ></input>
-        <label>Confirm Password</label>
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(event) => setConfirmPassword(event.target.value)}
-        ></input>
-        <p>Role</p>
-        <label>Hacker</label>
-        <input
-          type="radio"
-          name="role"
-          value="Hacker"
-          onChange={(event) => setRole(event.target.value)}
-        ></input>
-        <label>Hipster</label>
-        <input
-          type="radio"
-          name="role"
-          value="Hipster"
-          onChange={(event) => setRole(event.target.value)}
-        ></input>
-        <label>Hustler</label>
-        <input
-          type="radio"
-          name="role"
-          value="Hustler"
-          onChange={(event) => setRole(event.target.value)}
-        ></input>
-        <button type="submit" onClick={submitForm}>
+    <Container>
+      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPasswordConfirm">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPasswordConfirm">
+          <Form.Label>Team Role</Form.Label>
+          {["radio"].map((type) => (
+            <div key={`inline-${type}`} className="mb-3">
+              <Form.Check
+                inline
+                name="Role"
+                label="Hacker"
+                type={type}
+                id={`inline-${type}-1`}
+                value="Hacker"
+                onChange={(event) => setRole(event.target.value)}
+              />
+              <Form.Check
+                name="Role"
+                inline
+                label="Hustler"
+                type={type}
+                id={`inline-${type}-2`}
+                value="Hustler"
+                onChange={(event) => setRole(event.target.value)}
+              />
+              <Form.Check
+                name="Role"
+                inline
+                label="Hipster"
+                type={type}
+                id={`inline-${type}-3`}
+                value="Hipster"
+                onChange={(event) => setRole(event.target.value)}
+              />
+            </div>
+          ))}
+        </Form.Group>
+        <Button type="submit" onClick={submitForm}>
           Sign Up!
-        </button>
-      </form>
-    </div>
+        </Button>
+      </Form>
+      <p>
+        Already a member? <Link to="/login">Log in</Link> here.
+      </p>
+    </Container>
   );
 }
