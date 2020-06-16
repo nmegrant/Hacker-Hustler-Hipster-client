75ed214c7d8b3895c9d3a0c80082ede02fe26615
nmegrant <nme.grant@gmail.com>
2020-06-16 12:37:26 +0200

    Add styling to forms for sign in and log in

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,6 +4,11 @@ import { Link, useHistory } from "react-router-dom";
 import { loginThunkCreator } from "../store/user/actions";
 import { selectToken } from "../store/user/selectors";
 
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Col } from "react-bootstrap";
+
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -25,27 +30,33 @@ export default function Login() {
   }
 
   return (
-    <div>
-      <form>
-        <label>email</label>
-        <input
-          type="text"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        ></input>
-        <label>password</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        ></input>
-        <button type="submit" onClick={submitForm}>
+    <Container>
+      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+        <Form.Group controlId="formGroupEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type="submit" onClick={submitForm}>
           Login
-        </button>
-      </form>
+        </Button>
+      </Form>
       <p>
         Not a member? Don't miss out! <Link to="/signup">Sign up now.</Link>
       </p>
-    </div>
+    </Container>
   );
 }
