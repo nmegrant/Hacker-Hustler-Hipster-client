0bb3088ca2786805736cb32aa060e01f49d80824
nmegrant <nme.grant@gmail.com>
2020-06-16 14:52:28 +0200

    Style homepage cards

--- src/pages/Homepages.js
+++ src/pages/Homepages.js
@@ -4,6 +4,8 @@ import { fetchHomepagesThunkCreator } from "../store/homepages/actions";
 import { selectHomepages } from "../store/homepages/selectors";
 import HomepageCard from "../components/HomepageCard";
 
+import Container from "react-bootstrap/Container";
+
 export default function Homepages() {
   const dispatch = useDispatch();
 
@@ -14,11 +16,12 @@ export default function Homepages() {
   const homepages = useSelector(selectHomepages());
 
   return (
-    <div>
+    <Container>
       {homepages.map((homepage) => {
         return (
           <HomepageCard
             key={homepage.id}
+            role={homepage.user.role}
             userId={homepage.user.id}
             name={homepage.user.name}
             byline={homepage.byline}
@@ -26,6 +29,6 @@ export default function Homepages() {
           />
         );
       })}
-    </div>
+    </Container>
   );
 }
