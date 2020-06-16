8b1f8c7e84795c725e2eb00d11c4e84795737b9f
nmegrant <nme.grant@gmail.com>
2020-06-16 15:28:58 +0200

    Add my page and add some styling

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { selectHomepageDetails } from "../store/homepageDetails/selectors";
+
+import { Tabs, Tab } from "react-bootstrap/";
 
 export default function MyPage() {
   return (
-    <div>
-      <h1>User!</h1>
-    </div>
+    <Tabs defaultActiveKey="homepage" id="uncontrolled-tab-example">
+      <Tab eventKey="homepage" title="My Homepage">
+        <h1>Homepage stuff</h1>
+      </Tab>
+      <Tab eventKey="edit" title="Edit">
+        <h1>Form to edit homepage</h1>
+      </Tab>
+      <Tab eventKey="ideas" title="Ideas">
+        <h1>Ideas and a form to add ideas</h1>
+      </Tab>
+    </Tabs>
   );
 }
