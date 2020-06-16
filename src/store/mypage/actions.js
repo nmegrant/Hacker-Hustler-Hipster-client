19af2c7339d2c87e2dc1035923eb599adf6a762c
nmegrant <nme.grant@gmail.com>
2020-06-17 00:07:37 +0200

    Dispatch homepage info to API endpoint

--- src/store/mypage/actions.js
+++ src/store/mypage/actions.js
@@ -8,4 +8,22 @@ export function sendHomepageInfoThunkCreator(
   idea,
   location,
   urls
-) {}
+) {
+  return async function sendInfoThunk(dispatch, getState) {
+    const token = localStorage.getItem("token");
+    if (token === null) {
+      return;
+    }
+    try {
+      const myHomepageResponse = await axios.patch(
+        `http://localhost:4000/mypage/`,
+        { byline, experience, bio, idea, location },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  };
+}
