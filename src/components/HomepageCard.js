668aa35989e582cf45f8fb4b4730ac5fd565eb8a
nmegrant <nme.grant@gmail.com>
2020-06-16 14:52:28 +0200

    Style homepage cards

--- src/components/HomepageCard.js
+++ src/components/HomepageCard.js
@@ -1,21 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Badge from "react-bootstrap/Badge";
+// import Container from "react-bootstrap/Container";
+// import { Row } from "react-bootstrap";
+
 export default function HomepageCard(props) {
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h5>{props.byline}</h5>
-      {props.tags.map((tag) => {
-        return (
-          <div key={tag.id}>
-            <p>{tag.skill}</p>
-          </div>
-        );
-      })}
-      <button>
-        <Link to={`/homepages/${props.userId}`}>Show me more</Link>
-      </button>
-    </div>
+    <Card style={{ width: "20rem" }}>
+      <Card.Body>
+        <Card.Title>
+          {props.name} - {props.role}
+        </Card.Title>
+        <Card.Text>{props.byline}</Card.Text>
+
+        {props.tags.map((tag) => {
+          return (
+            <span key={tag.id}>
+              <Badge
+                pill
+                variant="info"
+                style={{ marginLeft: "5px", marginRight: "5px" }}
+              >
+                {tag.skill}
+              </Badge>
+            </span>
+          );
+        })}
+      </Card.Body>
+      <Card.Footer>
+        <Button>
+          <Link to={`/homepages/${props.userId}`} style={{ color: "white" }}>
+            Show me more
+          </Link>
+        </Button>
+      </Card.Footer>
+    </Card>
   );
 }
