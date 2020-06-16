145b0b92a803ecc02b2ac0e4947aa2a537640a15
nmegrant <nme.grant@gmail.com>
2020-06-16 18:15:37 +0200

    Fix error in using homepageId not userId to get homepage details

--- src/components/HomepageCard.js
+++ src/components/HomepageCard.js
@@ -7,7 +7,7 @@ import Badge from "react-bootstrap/Badge";
 
 export default function HomepageCard(props) {
   return (
-    <Card style={{ width: "20rem" }} border="info">
+    <Card style={{ width: "20px" }} border="info">
       <Card.Body>
         <Card.Title>
           {props.name} - {props.role}
@@ -30,7 +30,10 @@ export default function HomepageCard(props) {
       </Card.Body>
       <Card.Footer>
         <Button>
-          <Link to={`/homepages/${props.userId}`} style={{ color: "white" }}>
+          <Link
+            to={`/homepages/${props.homepageId}`}
+            style={{ color: "white" }}
+          >
             Show me more
           </Link>
         </Button>
