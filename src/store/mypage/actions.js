69bf47976a424f10e8e844c809b5e8778402d89e
nmegrant <nme.grant@gmail.com>
2020-06-16 23:42:06 +0200

    Send info to action thunk creator

---
+++ src/store/mypage/actions.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { selectToken } from "./selectors";
+
+export function sendHomepageInfoThunkCreator(
+  byline,
+  experience,
+  bio,
+  idea,
+  location,
+  urls
+) {}
