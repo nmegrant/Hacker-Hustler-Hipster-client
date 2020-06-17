66c2bdf86631ee8577e408050effa85cde98b98e
nmegrant <nme.grant@gmail.com>
2020-06-17 12:39:29 +0200

    Get ideas from API successful

--- src/store/ideas/reducer.js
+++ src/store/ideas/reducer.js
@@ -1,11 +1,9 @@
-const initialState = {};
+const initialState = [];
 
 export default function ideasReducer(state = initialState, action) {
   switch (action.type) {
     case "FETCH_IDEAS":
-      return {
-        ...action.payload,
-      };
+      return [...action.payload];
     default:
       return state;
   }
