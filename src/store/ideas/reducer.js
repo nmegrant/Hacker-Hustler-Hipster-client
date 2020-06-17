6f60163bfcbab45ca3d403ee4fc1e59fd839abc5
nmegrant <nme.grant@gmail.com>
2020-06-17 12:36:14 +0200

    Create reducer,selector, update rootreducer

---
+++ src/store/ideas/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {};
+
+export default function ideasReducer(state = initialState, action) {
+  switch (action.type) {
+    case "FETCH_IDEAS":
+      return {
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+}
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -3,6 +3,7 @@ import homepages from "./homepages/reducer";
 import homepageDetails from "./homepageDetails/reducer";
 import user from "./user/reducer";
 import myPage from "./mypage/reducer";
+import ideas from "./ideas/reducer";
 
 export default combineReducers({
   homepages,
