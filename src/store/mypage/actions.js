f06ec19fae8217943b73f4a6cba9205fcb313c1f
nmegrant <nme.grant@gmail.com>
2020-06-17 09:47:09 +0200

    Add data model to repo

--- src/store/mypage/actions.js
+++ src/store/mypage/actions.js
@@ -10,26 +10,29 @@ export function sendHomepageInfoThunkCreator(
 ) {
   return async function sendInfoThunk(dispatch, getState) {
     const token = localStorage.getItem("token");
+
     if (token === null) {
       return;
     }
     try {
-      const myHomepageResponse = await axios.patch(
-        `http://localhost:4000/mypage/`,
-        { byline, experience, bio, idea, location },
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
-      if (urls) {
-        const newWebsites = await axios.post(
-          `http://localhost:4000/websites/`,
-          urls,
-          {
-            headers: { Authorization: `Bearer ${token}` },
-          }
-        );
-      }
+      //   const myHomepageResponse = await axios.patch(
+      //     `http://localhost:4000/mypage/`,
+      //     { byline, experience, bio, idea, location },
+      //     {
+      //       headers: { Authorization: `Bearer ${token}` },
+      //     }
+      //   );
+      console.log(urls);
+      //   if (urls) {
+      //     console.log("Here!");
+      //     const newWebsites = await axios.post(
+      //       `http://localhost:4000/websites/`,
+      //       urls,
+      //       {
+      //         headers: { Authorization: `Bearer ${token}` },
+      //       }
+      //     );
+      //   }
     } catch (error) {
       console.log(`Error: ${error}`);
     }
