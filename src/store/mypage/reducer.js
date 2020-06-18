d5fc8b21bce09a80759150d69f3fc0dae7b48ba0
nmegrant <nme.grant@gmail.com>
2020-06-18 09:45:40 +0200

    Fix need for refresh

--- src/store/mypage/reducer.js
+++ src/store/mypage/reducer.js
@@ -4,6 +4,8 @@ export default function homepageDetailsReducer(state = initialState, action) {
   switch (action.type) {
     case "FETCHED_MY_PAGE_DETAILS":
       return { ...action.payload };
+    case "UPDATE_MY_PAGE_DETAILS":
+      return { ...state, ...action.payload };
     default:
       return state;
   }
