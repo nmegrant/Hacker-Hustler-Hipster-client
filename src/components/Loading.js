02b3a88e075e20c50b1a0e4cd18395b9d7962418
nmegrant <nme.grant@gmail.com>
2020-06-20 13:24:32 +0200

    Centre loading spinner

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,20 @@ import Spinner from "react-bootstrap/Spinner";
 
 export default function Loading() {
   return (
-    <div className="loading_spinner">
+    <div
+      className="loading_spinner"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+        position: "fixed",
+        top: "0",
+        bottom: "0",
+        left: "0",
+        right: "0",
+      }}
+    >
       <Spinner animation="grow" variant="info"></Spinner>
     </div>
   );
