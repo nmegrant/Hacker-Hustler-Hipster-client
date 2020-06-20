324d1ade397d8cfbc9ecfaf1e0dd5c1ed258ad61
nmegrant <nme.grant@gmail.com>
2020-06-20 13:11:20 +0200

    Add loading/done loading action to my page, details, homepages

--- src/store/homepageDetails/actions.js
+++ src/store/homepageDetails/actions.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { showMessageThunkCreator } from "../appState/actions";
+import { appLoading, appDoneLoading } from "../appState/actions";
 
 export function homepageDetailsFetched(details) {
   return {
@@ -11,10 +12,12 @@ export function homepageDetailsFetched(details) {
 export function fetchHomepageDetailsThunkCreator(id) {
   return async function fetchHomepageDetailsThunk(dispatch, getState) {
     try {
+      dispatch(appLoading());
       const homepageDetails = await axios.get(
         `http://localhost:4000/homepages/${id}`
       );
       dispatch(homepageDetailsFetched(homepageDetails.data));
+      dispatch(appDoneLoading());
     } catch (error) {
       console.log(`Error: ${error}`);
       dispatch(showMessageThunkCreator(error.response.data.message, "danger"));
