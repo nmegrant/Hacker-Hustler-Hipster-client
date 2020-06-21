b0d164e391928a8319a77dfe8d6e929b0d9ce104
nmegrant <nme.grant@gmail.com>
2020-06-21 17:42:19 +0200

    Add view of badges when editting homepage, click to delete, shake before delete

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import EditHomepage from "../components/EditHomepage";
+import EditHomepage from "../components/EditHomepage/EditHomepage";
 import MyHomepage from "../components/MyHomepage";
 import Ideas from "../components/Ideas";
 import MyPageBanner from "../components/MyPageBanner";
