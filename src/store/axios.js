eec66bd5dcbd6a39cb0985a557a70fdce19c951b
nmegrant <nme.grant@gmail.com>
2020-07-04 22:28:25 +0200

    Fix endpoint in axios.js

--- src/store/axios.js
+++ src/store/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: process.env.REACT_APP_HEROKU || "http://localhost:4000",
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.REACT_APP_HEROKU || "http://localhost:4000",
 });
