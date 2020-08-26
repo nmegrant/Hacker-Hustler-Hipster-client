602d248a5cb77e46b7b2fff027e27a78054b96dd
nmegrant <nme.grant@gmail.com>
2020-08-26 22:42:06 +0200

    Re-set axios endpoint

--- src/store/axios.js
+++ src/store/axios.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-//fixed
 export default axios.create({
-  // baseURL: process.env.REACT_APP_HEROKU || "http://localhost:4000",
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.REACT_APP_HEROKU || "http://localhost:4000",
 });
